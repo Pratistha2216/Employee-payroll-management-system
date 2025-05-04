f15026748dfe6e447580a71a3bd119e860f6bdc2
Pratistha2216 <pratistha.nigam2004@gmail.com>
2025-05-05 00:17:40 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,18 @@
+sample=f'''\tcompany name,XYZ\n\tAddess:Xyz,Floor4
+-----------------------------------------------------
+Employee ID\t\t:  1024
+Salary Of\t\t:  Dec-2020
+Generated on\t\t:  20-12-2020
+------------------------------------------------------
+Total days\t\t:  31
+Total present\t\t:  20
+Total Absent\t\t:  10
+Convernce\t\t:  Rs.1000
+Medical\t\t:  Rs.2000
+PF\t\t:  Rs.1500
+Gross Payment\t\t:  Rs.3562562
+Net Salary\t\t:  Rs.25455222
+--------------------------------------------------------
+This is computer generated slip,not
+requried any signature
+'''
